6dfc500fae81ee9eca181edecc9d7e5ad185f4de
Damjan15 <stamenovdamjan5@gmail.com>
2023-10-02 11:38:22 -0700

    feat: added ThemeProvider for switching between light and dark mode, shadcn installed, navbar setup for desktop and larger devices

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,6 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
       <h1>Home</h1>
     </div>
   );
---
+++ components/shared/navbar/Navbar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Theme from "./Theme";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/assets/images/site-logo.svg"
+          width={23}
+          height={23}
+          alt="DevFlow"
+        />
+
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900">
+          Dev <span className="text-primary-500">OverFlow</span>
+        </p>
+      </Link>
+
+      {/* @todo -> GlobalSearch Component */}
+
+      <div className="flex-between gap-5">
+        <Theme />
+
+        <SignedIn>
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+          />
+        </SignedIn>
+
+        {/* @todo -> MobileNavigation Bar */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
