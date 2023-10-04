a5f886951973d827dd9ccd0e4b7c00c69443c49c
Damjan15 <stamenovdamjan5@gmail.com>
2023-10-04 07:34:37 -0700

    feat: Question Form component setup, Questions schema setup

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Question from "@/components/forms/Question";
 
 const AskQuestion = () => {
-  return <div>AskQuestion</div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestion;
