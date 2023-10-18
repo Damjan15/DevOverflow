9e06ee299375e9e7356f1e095bb33bfa93e7d7a7
Damjan15 <stamenovdamjan5@gmail.com>
2023-10-18 02:22:05 -0700

    fix: fixed webhook secret name

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -8,7 +8,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   // @todo -> add your webhook secret to .env.local
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
