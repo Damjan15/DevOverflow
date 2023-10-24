49db17e77c8b8a162ee6b0e04f72db01f82d0a97
Damjan15 <stamenovdamjan5@gmail.com>
2023-10-24 11:44:25 -0700

    feat: search functionality implemented for the community page

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -3,9 +3,13 @@ import Link from "next/link";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { getAllUsers } from "@/lib/actions/user.action";
 import UserCard from "@/components/cards/UserCard";
+import { SearchParamsProps } from "@/types";
 
-const Community = async () => {
-  const result = await getAllUsers({});
+const Community = async ({ searchParams }: SearchParamsProps) => {
+  const result = await getAllUsers({
+    searchQuery: searchParams.q,
+  });
+
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">All Users</h1>
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -104,8 +104,17 @@ export async function getAllUsers(params: GetAllUsersParams) {
   try {
     connectToDatabase();
 
-    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
-    const users = await User.find({}).sort({ createdAt: -1 });
+    const { searchQuery } = params;
+    const query: FilterQuery<typeof User> = {};
+
+    if (searchQuery) {
+      query.$or = [
+        { name: { $regex: new RegExp(searchQuery, "i") } },
+        { username: { $regex: new RegExp(searchQuery, "i") } },
+      ];
+    }
+
+    const users = await User.find(query).sort({ createdAt: -1 });
     return { users };
   } catch (error) {
     console.log(error);
