7a71324567816dd17b573223cc21200d8d3baf7a
Damjan15 <stamenovdamjan5@gmail.com>
2023-10-26 03:52:59 -0700

    feat: pagination component created

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -8,6 +8,7 @@ import { HomePageFilters } from "@/constants/filters";
 import QuestionCard from "@/components/cards/QuestionCard";
 import { getQuestions } from "@/lib/actions/question.action";
 import { SearchParamsProps } from "@/types";
+import Pagination from "@/components/shared/Pagination";
 
 export default async function Home({ searchParams }: SearchParamsProps) {
   const results = await getQuestions({
@@ -70,6 +71,13 @@ export default async function Home({ searchParams }: SearchParamsProps) {
             linkTitle="Ask Question"
           />
         )}
+      </div>
+
+      <div className="mt-10">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={results.isNext}
+        />
       </div>
     </>
   );
