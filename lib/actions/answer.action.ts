8f48097c3a4965d8dbd0810e19025084669e8dae
Damjan15 <stamenovdamjan5@gmail.com>
2023-10-26 10:37:02 -0700

    feat: implemented reputation points for Answers

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -11,6 +11,7 @@ import {
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.modal";
 import Interaction from "@/database/interaction.modal";
+import User from "@/database/user.modal";
 
 export async function createAnswer(params: CreateAnswerParams) {
   try {
@@ -21,11 +22,19 @@ export async function createAnswer(params: CreateAnswerParams) {
     const newAnswer = await Answer.create({ content, author, question });
 
     // Add the answer to the question's answers array
-    await Question.findByIdAndUpdate(question, {
+    const questionObject = await Question.findByIdAndUpdate(question, {
       $push: { answers: newAnswer._id },
     });
 
-    // TODO: Add interaction...
+    await Interaction.create({
+      user: author,
+      action: "answer",
+      question,
+      answer: newAnswer._id,
+      tags: questionObject.tags,
+    });
+
+    await User.findByIdAndUpdate(author, { $inc: { reputation: 10 } });
 
     revalidatePath(path);
   } catch (error) {
@@ -94,7 +103,13 @@ export async function upvoteAnswers(params: AnswerVoteParams) {
       throw new Error("Answer not found");
     }
 
-    // @todo -> Increment author's reputation
+    await User.findByIdAndUpdate(userId, {
+      $inc: { reputation: hasupVoted ? -2 : 2 },
+    });
+
+    await User.findByIdAndUpdate(answer.author, {
+      $inc: { reputation: hasupVoted ? -10 : 10 },
+    });
 
     revalidatePath(path);
   } catch (error) {
@@ -130,7 +145,13 @@ export async function downvoteAnswers(params: AnswerVoteParams) {
       throw new Error("Question not found");
     }
 
-    // @todo -> Increment author's reputation
+    await User.findByIdAndUpdate(userId, {
+      $inc: { reputation: hasdownVoted ? -2 : 2 },
+    });
+
+    await User.findByIdAndUpdate(answer.author, {
+      $inc: { reputation: hasdownVoted ? -10 : 10 },
+    });
 
     revalidatePath(path);
   } catch (error) {
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -204,7 +204,13 @@ export async function downvoteQuestion(params: QuestionVoteParams) {
       throw new Error("Question not found");
     }
 
-    // @todo -> Increment author's reputation
+    await User.findByIdAndUpdate(userId, {
+      $inc: { reputation: hasdownVoted ? -2 : 2 },
+    });
+
+    await User.findByIdAndUpdate(question.author, {
+      $inc: { reputation: hasdownVoted ? -10 : 10 },
+    });
 
     revalidatePath(path);
   } catch (error) {
