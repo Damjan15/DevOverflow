32e213cee7d523097e532ed96a7fda5389049ff5
Damjan15 <stamenovdamjan5@gmail.com>
2023-10-26 00:12:15 -0700

    feat: managing filter state

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,16 +1,44 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { HomePageFilters } from "@/constants/filters";
 import { Button } from "../ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-  const active = "newest";
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [active, setActive] = useState("");
+
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive("");
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item.toLocaleLowerCase(),
+      });
+
+      router.push(newUrl, { scroll: false });
+    }
+  };
+
   return (
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
       {HomePageFilters.map((item) => (
         <Button
           key={item.value}
-          onClick={() => {}}
+          onClick={() => handleTypeClick(item.value)}
           className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
             active === item.value
               ? "bg-primary-100 text-primary-500"
--- components/shared/search/LocalSearchBar.tsx
+++ components/shared/search/LocalSearchBar.tsx
@@ -53,7 +53,6 @@ const LocalSearchBar = ({
     return () => clearTimeout(delayDebounceFn);
   }, [search, route, pathname, router, searchParams, query]);
 
-  console.log(query);
   return (
     <div
       className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
