74e2a7a9245be46ee1698b7d2bbe5a26cdc17816
Damjan15 <stamenovdamjan5@gmail.com>
2023-10-27 03:37:04 -0700

    feat: displaying the AI results on the UI

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -79,7 +79,15 @@ const Answer = ({ question, questionId, authorId }: Props) => {
 
       const aiAnswer = await response.json();
 
-      alert(aiAnswer.reply);
+      // Convert plain text into HTML format
+      const formattedAnswer = aiAnswer.reply.replace(/\n/g, "<br />");
+
+      if (editorRef.current) {
+        const editor = editorRef.current as any;
+        editor.setContent(formattedAnswer);
+      }
+
+      // @todo -> Toast notifications
     } catch (error) {
       console.log(error);
     } finally {
@@ -98,14 +106,20 @@ const Answer = ({ question, questionId, authorId }: Props) => {
           className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
           onClick={generateAIAnswer}
         >
-          <Image
-            src="/assets/icons/stars.svg"
-            alt="star"
-            width={12}
-            height={12}
-            className="object-contain"
-          />
-          Generate an AI Answer
+          {isSubmittingAI ? (
+            <>Generating...</>
+          ) : (
+            <>
+              <Image
+                src="/assets/icons/stars.svg"
+                alt="star"
+                width={12}
+                height={12}
+                className="object-contain"
+              />
+              Generate an AI Answer
+            </>
+          )}
         </Button>
       </div>
 
